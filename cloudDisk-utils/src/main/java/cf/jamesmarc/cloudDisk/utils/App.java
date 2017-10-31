51511d5332762c480953c0c7f76af56268ccb7eb
jamesmarc2016 <934396065@qq.com>
2017-10-31 11:14:43 +0800

    test

---
+++ cloudDisk-utils/src/main/java/cf/jamesmarc/cloudDisk/utils/App.java
@@ -0,0 +1,13 @@
+package cf.jamesmarc.cloudDisk.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
